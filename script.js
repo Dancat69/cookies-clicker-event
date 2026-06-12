1a5a92fec686cc3b3aab39829fc2fe707a2b4861
Dancat69 <danielsimonak@gmail.com>
2026-06-12 09:59:44 +0200

    final

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ const upgrades = [
     new Upgrade("clicker", "Clicker", "Multiplies cookies per click", "res/upgrade_icons/clicker.png", 15, 0),
     new Upgrade("flower", "VictorFlower", "Nice flower does cool stuff", "res/upgrade_icons/flower.png", 45, 0),
     new Upgrade("kid", "VictorKid", "Clicks 4x automatically but weakens your click by 1", "res/upgrade_icons/kid-victor.png", 85, 0),
-    new Upgrade("gym", "VictorGym", "Trains your click for +3 Victors per click", "res/upgrade_icons/victorGym.png", 100, 0),
-    new Upgrade("garden", "VictorGarden", "Grows +10 Victors automatically every second", "res/upgrade_icons/victorGarden.png", 175, 0),
-    new Upgrade("factory", "VictorFactory", "Produces +50 Victors per second and +2 Victors per click", "res/upgrade_icons/victorFactory.png", 225, 0),
+    new Upgrade("gym", "VictorGym", "Trains your click for +3 Coins per click", "res/upgrade_icons/victorGym.png", 100, 0),
+    new Upgrade("garden", "VictorHomeless", "Collects form the street +10 Coins automatically every second", "res/upgrade_icons/kid-victor.png", 175, 0),
+    new Upgrade("factory", "VictorFactory", "Produces +50 Coins per second and +2 Coins per click", "res/TOVARNA.png", 225, 0),
 ];
 
 // Unlock thresholds
@@ -38,7 +38,7 @@ const rateText = document.querySelector(".rate_text");
 const cookieContainer = document.querySelector(".cookie_container");
 const rebirthButton = document.getElementById("rebirth_button");
 
-let cookieCount = 1000000000;
+let cookieCount = 0;
 let cookiesPerSecond = 0;
 let clickMultiplier = 1;
 let totalClicks = 0;
@@ -559,8 +559,8 @@ function addHandAroundCookie() {
 
 const farmBackgrounds = {
     flower:  "res/field.png",
-    kid:     "res/field.png",
-    garden:  "res/field.png",
+    kid:     "res/school.png",
+    garden:  "res/street.png",
     factory: "res/field.png",
 };
 
